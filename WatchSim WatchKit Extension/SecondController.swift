16b49e12d7c0f94b9a071fba0de59cf41470b220
Ben Gottlieb <ben@retina-imac-2.local>
2015-02-11 11:58:53 -0600

    added WatchKit extension project to main project for testing

---
+++ WatchSim WatchKit Extension/SecondController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondController.swift
+//  WatchSim
+//
+//  Created by Ben Gottlieb on 2/11/15.
+//  Copyright (c) 2015 Stand Alone, inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondController: WKInterfaceParent {
+   
+}
